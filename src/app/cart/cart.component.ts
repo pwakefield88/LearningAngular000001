3feee54226e571cf586d7467020e1b3c2f3f3bc8
pwakefield88 <paul.wakefield@soprasteria.com>
2019-11-01 08:27:45 +0000

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,7 +11,8 @@ export class CartComponent implements OnInit {
   items;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
