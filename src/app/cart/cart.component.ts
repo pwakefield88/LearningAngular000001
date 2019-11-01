396697f3f07bbad345b8b73e99c63b964c8bfd66
pwakefield88 <paul.wakefield@soprasteria.com>
2019-11-01 08:30:17 +0000

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,7 +14,21 @@ export class CartComponent implements OnInit {
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
-  ) { }
+  ) { 
+    this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData){
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
