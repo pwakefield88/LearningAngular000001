6d152a2413e3405f8b19ad7e72bdd4c1ee7c6c76
pwakefield88 <paul.wakefield@soprasteria.com>
2019-11-01 08:05:12 +0000

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -15,6 +15,9 @@ export class ProductDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    })
   }
 
 }
